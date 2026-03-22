d585b3ae09500a48255e379395be5dd18edfd159
digarcia0603 <a107328@uminho.pt>
2026-03-22 18:01:48 +0000

    Fase1

---
+++ Fase1/build/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/engine.cpp.o"
+  "CMakeFiles/engine.dir/engine.cpp.o.d"
+  "CMakeFiles/engine.dir/tinyxml/tinyxml2.cpp.o"
+  "CMakeFiles/engine.dir/tinyxml/tinyxml2.cpp.o.d"
+  "engine"
+  "engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
